68dd18535dec3df2ede54c96727268ddc0067011
Ian Jury <mrianjury@gmail.com>
2017-04-19 19:03:03 -0700

    Fixed exec commands in test file

--- test.c
+++ test.c
@@ -19,24 +19,43 @@ int main(int argc, char const *argv[]) {
 
   p1 = fork();
 
-  if (p1 == 0) { //child process
+  if (p1 == 0) {              //child process 1
   char *myargs[3];
     myargs[0] = strdup("wc"); // program: "wc" (word count)
     myargs[1] = strdup("test.txt"); // argument: file to count
     myargs[2] = NULL; // marks end of array
     execvp(myargs[0], myargs); // runs word count
-    printf("Child process 1. PID = %d\n", getpid());
-  } if (p1 > 0) { //parent process
-    wait(NULL);
-    printf("Parent process 1. PID = %d. Child process = %d\n", getpid(), p1);
-    p2 = fork();
-    if (p2 == 0) { //child process 2
-      printf("Child process 2. PID = %d\n", getpid());
-    } if (p2 > 0) { //parent process 2
-      wait(NULL);
-      printf("Parent process 2. PID = %d. Child process = %d\n", getpid(), p2);
-    }
+    //printf("Child process 1. PID = %d\n", getpid());
 
+  } if (p1 > 0) { //parent process
+    p2 = fork();
+
+    if (p2 == 0) { //child process 2
+      char *myargs[3];
+      myargs[0] = strdup("ls");
+      myargs[1] = strdup("test.txt"); // argument: file to count
+      myargs[2] = NULL; // marks end of array
+      execvp(myargs[0], myargs);
+
+    } if (p2 > 0) { //parent process 2
+      p3 = fork();
+      wait(NULL);
+      if (p3 == 0) {   // Child process 3
+        char *myargs[3];
+        myargs[0] = strdup("wc");
+        myargs[1] = strdup("test.txt"); // argument: file to count
+        myargs[2] = NULL; // marks end of array
+        //printf("!!!!Child process 2. PID = %d\n", getpid());
+        execvp(myargs[0], myargs);
+      }
+      if (p3 > 0) { //parent process 3
+        wait(NULL);
+      }
+      //wait(NULL);
+      //printf("Parent process 2. PID = %d. Child process = %d\n", getpid(), p2);
+    }
+    wait(NULL); //first parent waits for all children
+    printf("Finished waiting for child processes %d %d %d\n", p1, p2, p3);
   }
 
 
